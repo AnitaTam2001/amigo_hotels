b5c3ad9703efc7b92339d3258cf860e7164bd2b7
Anita <anitatam2001@gmail.com>
2025-10-21 17:08:17 +0800

    Step 3 : update files

--- hotels/urls.py
+++ hotels/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('hotels/', views.hotel_list, name='hotel_list'),
     path('hotels/<int:hotel_id>/', views.hotel_detail, name='hotel_detail'),
     path('search/', views.search_hotels, name='search_hotels'),
+    path('book/<int:hotel_id>/', views.book_hotel, name='book_hotel'),
+    path('booking-success/<int:booking_id>/', views.booking_success, name='booking_success'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
 ]
--- hotels/views.py
+++ hotels/views.py
@@ -2,6 +2,29 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from .models import Hotel
 
+FAQ_DATA = [
+    {
+        'question': 'What is your cancellation policy?',
+        'answer': 'Free cancellation up to 24 hours before check-in.'
+    },
+    {
+        'question': 'Do you offer airport transportation?',
+        'answer': 'Yes, we offer airport transportation at most locations.'
+    },
+    {
+        'question': 'Are pets allowed?',
+        'answer': 'Some of our Amigo Hotels are pet-friendly.'
+    },
+    {
+        'question': 'What time is check-in and check-out?',
+        'answer': 'Check-in: 3:00 PM, Check-out: 11:00 AM.'
+    },
+    {
+        'question': 'Do you have swimming pools?',
+        'answer': 'Most of our Amigo Hotels feature swimming pools.'
+    }
+]
+
 def home(request):
     hotels = Hotel.objects.all()
     return render(request, 'home.html', {'hotels': hotels})
@@ -30,8 +53,18 @@ def search_hotels(request):
         'query': query
     })
 
+def book_hotel(request, hotel_id):
+    hotel = get_object_or_404(Hotel, id=hotel_id)
+    return render(request, 'hotels/booking.html', {'hotel': hotel})
+
+def booking_success(request, booking_id):
+    return render(request, 'hotels/booking_success.html')
+
 def about(request):
     return render(request, 'about.html')
 
 def contact(request):
     return render(request, 'contact.html')
+
+def faq(request):
+    return render(request, 'faq.html', {'faqs': FAQ_DATA})
